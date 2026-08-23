67103c73adb08d866e6402be977e15e5021856cc
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-08-23 11:58:56 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,21 @@
           class="name-bone__body"
           d="M105 47C67 26 19 53 21 97C22 120 35 136 55 146C34 158 22 177 23 200C25 241 71 264 106 244C123 234 131 219 139 202C151 178 169 172 199 172H561C591 172 609 178 621 202C629 219 637 234 654 244C689 264 735 241 737 200C738 177 726 158 705 146C725 136 738 120 739 97C741 53 693 26 655 47C637 57 628 73 620 90C609 112 590 118 561 118H199C170 118 151 112 140 90C132 73 123 57 105 47Z"
         />
-        <text class="name-bone__text" x="380" y="153" text-anchor="middle">Pietra Sofia</text>
+        <text class="name-bone__text" x="380" y="153" text-anchor="middle">PIETRA SOFIA</text>
       </svg>
     </div>
   </main>
 </template>
 
 <style scoped>
+@font-face {
+  font-family: 'Pietra Sofia';
+  src: url('/font.ttf') format('truetype');
+  font-display: swap;
+  font-style: normal;
+  font-weight: normal;
+}
+
 .birthday-background {
   position: relative;
   min-height: 100svh;
@@ -70,9 +78,9 @@
 
 .name-bone__text {
   fill: #080808;
-  font-family: 'Arial Rounded MT Bold', 'Trebuchet MS', sans-serif;
+  font-family: 'Pietra Sofia', cursive;
   font-size: 70px;
-  font-weight: 900;
+  font-weight: normal;
   letter-spacing: -2px;
   paint-order: stroke;
   stroke: #fff9e8;
