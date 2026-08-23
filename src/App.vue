339cf8db34d99a046b29cb6eeb202d0d39d2c7a4
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-08-23 11:27:40 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="birthday-background" aria-label="Fundo decorativo laranja com sementes pretas">
+    <div class="birthday-background__texture" aria-hidden="true"></div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.birthday-background {
+  position: relative;
+  min-height: 100svh;
+  overflow: hidden;
+  background-color: #ff914d;
+  isolation: isolate;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.birthday-background__texture {
+  position: absolute;
+  inset: -45px;
+  background-image: url('/watermelon-pattern.svg');
+  background-position: center top;
+  background-size: 310px 310px;
+  transform: rotate(-0.4deg) scale(1.02);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+/* Uma granulação quase imperceptível tira o aspecto excessivamente digital. */
+.birthday-background::after {
+  position: absolute;
+  inset: 0;
+  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.9' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)' opacity='.18'/%3E%3C/svg%3E");
+  content: '';
+  opacity: 0.035;
+  pointer-events: none;
+  mix-blend-mode: multiply;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+@media (max-width: 600px) {
+  .birthday-background__texture {
+    background-size: 255px 255px;
   }
 }
 </style>
