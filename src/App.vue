d339aa35ea26586de7dbd54fb445d1c7159961eb
Ph4ra0hXX <ph4ra0hx@gmail.com>
2026-08-24 14:11:20 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <main class="birthday-background" aria-label="Fundo decorativo laranja com sementes pretas">
+  <main
+    class="birthday-background"
+    aria-label="Fundo decorativo laranja com sementes pretas"
+  >
     <div class="birthday-background__texture" aria-hidden="true"></div>
 
     <div class="name-bone" role="img" aria-label="Osso com o nome Pietra Sofia">
@@ -17,9 +20,13 @@
           class="name-bone__body"
           d="M105 47C67 26 19 53 21 97C22 120 35 136 55 146C34 158 22 177 23 200C25 241 71 264 106 244C123 234 131 219 139 202C151 178 169 172 199 172H561C591 172 609 178 621 202C629 219 637 234 654 244C689 264 735 241 737 200C738 177 726 158 705 146C725 136 738 120 739 97C741 53 693 26 655 47C637 57 628 73 620 90C609 112 590 118 561 118H199C170 118 151 112 140 90C132 73 123 57 105 47Z"
         />
-                <text class="name-bone__text" x="380" y="133" text-anchor="middle">1 ANO</text>
+        <text class="name-bone__text" x="380" y="133" text-anchor="middle">
+          1 ANO
+        </text>
 
-        <text class="name-bone__text" x="380" y="203" text-anchor="middle">PIETRA SOFIA</text>
+        <text class="name-bone__text" x="380" y="203" text-anchor="middle">
+          PIETRA SOFIA
+        </text>
       </svg>
     </div>
   </main>
@@ -27,8 +34,8 @@
 
 <style scoped>
 @font-face {
-  font-family: 'Pietra Sofia';
-  src: url('/font.ttf') format('truetype');
+  font-family: "Pietra Sofia";
+  src: url("/font.ttf") format("truetype");
   font-display: swap;
   font-style: normal;
   font-weight: normal;
@@ -45,7 +52,7 @@
 .birthday-background__texture {
   position: absolute;
   inset: -45px;
-  background-image: url('/watermelon-pattern.svg');
+  background-image: url("/watermelon-pattern.svg");
   background-position: center top;
   background-size: 310px 310px;
   transform: rotate(-0.4deg) scale(1.02);
@@ -94,13 +101,13 @@
 }
 
 .name-bone__text {
-  fill: #080808;
-  font-family: 'Pietra Sofia', cursive;
+  fill: #9fca3d;
+  font-family: "Pietra Sofia", cursive;
   font-size: 70px;
   font-weight: normal;
   letter-spacing: -2px;
   paint-order: stroke;
-  stroke: #fff9e8;
+  stroke: #000000;
   stroke-width: 12px;
 }
 
@@ -109,7 +116,7 @@
   position: absolute;
   inset: 0;
   background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='.9' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)' opacity='.18'/%3E%3C/svg%3E");
-  content: '';
+  content: "";
   opacity: 0.035;
   pointer-events: none;
   mix-blend-mode: multiply;
